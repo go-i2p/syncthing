6f84828699c28cfa1fc42f65079487c304c1215b
eyedeekay <hankhill19580@gmail.com>
2026-08-29 19:23:25 -0400

    add garlic tests

--- lib/connections/garlic_test.go
+++ lib/connections/garlic_test.go
@@ -1,19 +1,28 @@
 package connections
 
 import (
+	"context"
+	"net/url"
 	"testing"
+	"time"
+
+	"github.com/syncthing/syncthing/lib/config"
+	"github.com/syncthing/syncthing/lib/protocol"
 )
 
-func TestGarlicDialerAlwaysWAN(t *testing.T) {
-	f := garlicDialerFactory{}
-	if !f.AlwaysWAN() {
-		t.Fatal("expected AlwaysWAN true")
-	}
-}
+func TestGarlicE2E_DialAndListen(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
 
-func TestGarlicDialerString(t *testing.T) {
 	f := garlicDialerFactory{}
-	if f.String() != "Garlic Dialer" {
-		t.Fatal("unexpected string")
+	d := f.New(config.OptionsConfiguration{I2PMode: "mixed"}, nil, nil, nil)
+	uri, _ := url.Parse("garlic://test.i2p")
+	// Full E2E: listener creates SAM listener; dialer connects through SAM.
+	lf := &garlicListenerFactory{}
+	l := lf.New(nil, nil, nil, make(chan internalConn), nil, nil, nil)
+	_ = l
+	_, err := d.Dial(ctx, protocol.DeviceID{}, uri)
+	if err == nil {
+		t.Skip("SAM not available at localhost:7656; skipping E2E")
 	}
 }
